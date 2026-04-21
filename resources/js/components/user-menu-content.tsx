39f1d3918e568e27be33f7c9679d87d8aa8e424c
Sadirul <sadirul.islam786@gmail.com>
2026-04-22 00:44:44 +0530

    fixed

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -3,7 +3,7 @@ import { UserInfo } from '@/components/user-info';
 import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
 import { type User } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LogOut, Settings, UserRound } from 'lucide-react';
+import { LockKeyhole, LogOut, UserRound } from 'lucide-react';
 
 interface UserMenuContentProps {
     user: User;
@@ -29,8 +29,8 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
                     <Link className="block w-full" href={route('password.edit')} as="button" prefetch onClick={cleanup}>
-                        <Settings className="mr-2" />
-                        Setting
+                        <LockKeyhole className="mr-2" />
+                        Security
                     </Link>
                 </DropdownMenuItem>
             </DropdownMenuGroup>
